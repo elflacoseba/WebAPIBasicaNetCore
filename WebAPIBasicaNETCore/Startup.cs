c9469039118d9c9d0e49008c651f0b533a977e17
Sebastián Serrisuela <sebaserri@gmail.com>
2021-09-29 10:33:55 -0300

    Agregando el servicio y el middleware de MVC

--- WebAPIBasicaNETCore/Startup.cs
+++ WebAPIBasicaNETCore/Startup.cs
@@ -16,6 +16,8 @@ namespace WebAPIBasicaNETCore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,13 +30,29 @@ namespace WebAPIBasicaNETCore
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseStatusCodePages();
+
+            app.UseMvc();
+            
+            //app.UseMvc(config => 
+            //{
+            //    config.MapRoute(
+            //        name: "Default",
+            //        template: "{controller}/{action}/{id}",
+            //        defaults: new
+            //        {
+            //            controller = "Home",
+            //            action = "Index"
+            //        });
+            //});
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
